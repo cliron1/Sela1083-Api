3f5f4c49ba7b164719222b8d8800611c44bc20c8
liron <liron@flame-ware.com>
2023-07-26 09:40:06 +0300

    2023-07-24 (Azure)

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IConfiguration config, ILogger<HomeController> logger)
     {
         _logger = logger;
     }
